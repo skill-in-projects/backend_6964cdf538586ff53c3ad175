7fbacb24a7e4c2a2829365a27ecd115901930dc0
skill-in-projects <admin@skill-in.com>
2026-01-12 12:39:42 +0200

    Update TestController.py

--- Controllers/TestController.py
+++ Controllers/TestController.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from Models.TestProjects import TestProjects
 from psycopg import AsyncConnection
 from psycopg.rows import dict_row
-
+dfad
 router = APIRouter(prefix="/api/test", tags=["test"])
 
 async def get_db_connection():
